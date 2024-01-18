9d21416ba2a05abc8f8ee458ca9ca0376a3c50d7
Miroslav Tzarev <mtzarev@gmail.com>
2024-01-18 12:16:43 +0200

    Add files via upload

---
+++ 7. Required reading.js
@@ -0,0 +1,8 @@
+function requiredReading (numPages, pagesOneHour, daysForRead){
+
+let res = (numPages/pagesOneHour)/daysForRead
+    console.log(`${res}`);
+}
+//requiredReading (212,
+//   20 , /   2 )
+requiredReading(432, 15, 4 )
